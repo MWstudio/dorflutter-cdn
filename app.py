29fe71c4126223df3ded0b84bbd18693e3257da2
[minwoo <lmwljw96@gmail.com>
2022-06-24 19:25:54 +0900

    add

---
+++ app.py
@@ -0,0 +1,1 @@
+from image_server.main import app as application
---
+++ run.py
@@ -0,0 +1,5 @@
+from image_server.main import create_app
+
+if __name__ == '__main__':
+  app = create_app()
+  app.run(debug=True, host=app.config["HOST"], port=app.config["PORT"])
